a889a585f849e9d14dd0c4bb4b7c3fce4e9a457a
yq.liu <yq.liu@nercdtv.org>
2019-01-14 12:21:29 +0800

    copy .vimrc to home path automatically

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+CURPATH=`pwd`
+# copy .vimrc to home path
+cp -f ${CURPATH}/.vimrc ~/.vimrc
+
 # just install plug.vim, no more
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
