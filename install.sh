bec7f68198e954db67a60d892ae356ecfbb0fed7
Dante <liuyongqiangx@hotmail.com>
2019-07-27 00:00:51 +0800

    Add vim-plug to repo, do not download with curl

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 CURPATH=`pwd`
 # copy .vimrc to home path
+cp -rf ${CURPATH}/vim ~/.vim
 cp -f ${CURPATH}/.vimrc ~/.vimrc
 cp -f ${CURPATH}/.vimrc.local ~/.vimrc.local
 
-# just install plug.vim, no more
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# just install plug.vim, do not download used curl, add plug.vim in this repo
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+			#https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c "PlugInstall" -c "q" -c "q"
 
 
