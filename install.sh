3602fbe04cf979175cbc992b13fcda1b57528140
Dante <liuyongqiangx@hotmail.com>
2019-09-08 22:45:10 +0800

    Add install for neovim

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ cp -rf ${CURPATH}/vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/vimrc.local ~/.vimrc.local
 
+#for neovim
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vim ~/.config/nvim/init.vim
+
 # just install plug.vim, do not download used curl, add plug.vim in this repo
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 			#https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
