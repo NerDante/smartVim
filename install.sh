7024584dbfc1e5efdd7c7958e59f2bc7fd1e4628
NerDante <liuyongqiangx@hotmail.com>
2019-09-24 09:57:40 +0800

    rename vimrc files

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 CURPATH=`pwd`
 # copy .vimrc to home path
 cp -rf ${CURPATH}/vim ~/.vim
-cp -f ${CURPATH}/.vimrc ~/.vimrc
-cp -f ${CURPATH}/.vimrc.local ~/.vimrc.local
+cp -f ${CURPATH}/vimrc ~/.vimrc
+cp -f ${CURPATH}/vimrc.local ~/.vimrc.local
 
 # just install plug.vim, do not download used curl, add plug.vim in this repo
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
