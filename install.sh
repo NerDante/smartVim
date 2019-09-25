6da9fb91f27fa2dc605d76878f9b8e16125d83c7
NerDante <liuyongqiangx@hotmail.com>
2019-09-25 16:30:24 +0800

    Add some Nerd Fonts to repo

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 CURPATH=`pwd`
+platform_type=`echo $(uname)`
+
 # copy .vimrc to home path
 cp -rf ${CURPATH}/vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
@@ -10,9 +12,17 @@ ln -s ~/.vim/vimrc.local ~/.vimrc.local
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
 
+#install fonts
+if [ ${platform_type} == "Linux" ];then
+    cp  -f fonts/* ~/.local/share/fonts
+elif[ ${platform_type} == "Darwin" ];then
+    cp  -f fonts/* ~/Library/Fonts
+else
+    echo "not support platform type ${platform_type}"
+fi
+
 # just install plug.vim, do not download used curl, add plug.vim in this repo
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 			#https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c "PlugInstall" -c "q" -c "q"
-
 
